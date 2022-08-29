7216594e9b61f2236f2305ea91fcdb7b62ef5b0d
Hien Phan <31251608+HienPhanVN@users.noreply.github.com>
2022-08-29 14:04:42 +0700

    change waitress to gunicorn

--- run_in_background.sh
+++ run_in_background.sh
@@ -1,2 +1,2 @@
  cd /root/Flask_server/WOL_Flask_server
- nohup waitress-serve --host 0.0.0.0 app:app > log.txt 2>&1 & exit
+ nohup gunicorn --bind 0.0.0.0:8080 -w 4 app:app > flask_server_log.txt 2>&1 & exit
